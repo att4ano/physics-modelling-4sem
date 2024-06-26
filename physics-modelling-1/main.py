184c0054a515e5fa72db24c5a0b6aabad7c95a1e
Alex <alexmath.it@mail.ru>
2024-06-26 10:12:36 +0300

    dfghjkl;'

--- physics-modelling-1/main.py
+++ physics-modelling-1/main.py
@@ -2,53 +2,97 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def intensity(x, wavelength, L, d, b, N):
-    theta = np.arctan(x / L)
-    beta = (np.pi * d * np.sin(theta)) / wavelength
-    I = (np.sin(N * beta) / np.sin(beta)) ** 2 * (np.sin(beta / d * b) / (beta / d * b)) ** 2
-    return I
+def calculate_intensity(phi, slit_width, slit_separation, wavelength, num_slits):
+    return (np.sin(np.pi * slit_width / wavelength * np.sin(phi)) /
+            (np.sin(phi) * np.pi * slit_width / wavelength)) ** 2 * \
+        (np.sin(num_slits * np.pi * slit_separation / wavelength * np.sin(phi)) /
+         np.sin(np.sin(phi) * np.pi * slit_separation / wavelength)) ** 2
 
 
-def normalize(I):
-    return I / np.max(I)
+def wavelength_to_rgb(wavelength):
+    gamma = 0.8
+    intensity_max = 255
+    factor = 0.0
+    R = G = B = 0
+
+    if 380 <= wavelength <= 440:
+        R = -(wavelength - 440) / (440 - 380)
+        G = 0.0
+        B = 1.0
+    elif 440 <= wavelength <= 490:
+        R = 0.0
+        G = (wavelength - 440) / (490 - 440)
+        B = 1.0
+    elif 490 <= wavelength <= 510:
+        R = 0.0
+        G = 1.0
+        B = -(wavelength - 510) / (510 - 490)
+    elif 510 <= wavelength <= 580:
+        R = (wavelength - 510) / (580 - 510)
+        G = 1.0
+        B = 0.0
+    elif 580 <= wavelength <= 645:
+        R = 1.0
+        G = -(wavelength - 645) / (645 - 580)
+        B = 0.0
+    elif 645 <= wavelength <= 780:
+        R = 1.0
+        G = 0.0
+        B = 0.0
+
+    if 380 <= wavelength <= 420:
+        factor = 0.3 + 0.7 * (wavelength - 380) / (420 - 380)
+    elif 420 <= wavelength <= 645:
+        factor = 1.0
+    elif 645 <= wavelength <= 780:
+        factor = 0.3 + 0.7 * (780 - wavelength) / (780 - 645)
+
+    R = int(intensity_max * ((R * factor) ** gamma))
+    G = int(intensity_max * ((G * factor) ** gamma))
+    B = int(intensity_max * ((B * factor) ** gamma))
+
+    return R, G, B
 
 
-def calculate_total_intensity(x, wavelengths, L, d, b, N):
-    I_total = np.zeros_like(x)
-    for wavelength in wavelengths:
-        I_total += intensity(x, wavelength, L, d, b, N)
-    return I_total
+num_slits = 10
+slit_width = 100
+slit_separation = 200
+wavelength = 0.64
+factor = 2
+
+delta_lambda = wavelength / (num_slits * factor)
+lambda2 = wavelength + delta_lambda
+
+phi = np.linspace(-np.pi, np.pi, 1000000)
+
+intensity1 = calculate_intensity(phi, slit_width, slit_separation, wavelength, num_slits)
+intensity2 = calculate_intensity(phi, slit_width, slit_separation, lambda2, num_slits)
+
+tmp_intensity1 = calculate_intensity(-np.pi, slit_width, slit_separation, wavelength, num_slits)
+tmp_intensity2 = calculate_intensity(np.pi, slit_width, slit_separation, wavelength, num_slits)
+
+plt.figure(figsize=(10, 6))
+
+plt.plot(phi, intensity1, label=f'Intensity for λ = {wavelength:.2f} μm')
+plt.plot(phi, intensity2, label=f'Intensity for λ = {lambda2:.2f} μm')
+plt.xlim(-0.02, 0.02)
+plt.ylim(0, num_slits ** 2 / 2)
+plt.xlabel(r'$\varphi$, rad')
+plt.ylabel(r'Intensity')
+plt.legend()
+
+arr = []
+
+for i in phi:
+    arr.append(1 / calculate_intensity(i, slit_width, slit_separation, wavelength, num_slits))
 
 
-def plot_intensity(x, I_mono, I_total, lambda_0, delta_lambda):
-    plt.figure(figsize=(12, 6))
-    plt.plot(x, I_mono, label=f'Монохроматический свет ($\\lambda_0 = {lambda_0 * 1e9:.0f}$ нм)')
-    plt.plot(x, I_total, linestyle='--',
-             label=f'Квазимонохроматический свет ($\\lambda_0 = {lambda_0 * 1e9:.0f}$ нм, $\\Delta \\lambda = {delta_lambda * 1e9:.0f}$ нм)')
-    plt.xlabel('Координата x (м)')
-    plt.ylabel('Интенсивность')
-    plt.title('Дифракционная картина монохроматического и квазимонохроматического света')
-    plt.legend()
-    plt.grid(True)
-    plt.show()
+# wavelengths = np.linspace((1 / tmp_intensity1) * 1000, (1 / tmp_intensity2) * 1000, 1000)
+colors = [wavelength_to_rgb(wl) for wl in arr]
+color_map = np.array(colors).reshape(1, -1, 3)
 
-
-lambda_0 = 500e-9  # центральная длина волны в метрах
-delta_lambda = 300e-9  # спектральная ширина в метрах
-
-period = 1e-6  # период решетки в метрах
-height = 1e-6  # высота щели в метрах
-num_gaps = 1000  # число щелей
-
-L = 1.0  # расстояние до экрана в метрах
-
-x = np.linspace(-0.1, 0.1, 1000000)  # диапазон координат x в метрах
-
-I_mono = intensity(x, lambda_0, L, period, height, num_gaps)
-wavelengths = np.linspace(lambda_0 - delta_lambda / 2, lambda_0 + delta_lambda / 2, 100)
-I_total = calculate_total_intensity(x, wavelengths, L, period, height, num_gaps)
-
-I_mono = normalize(I_mono)
-I_total = normalize(I_total)
-
-plot_intensity(x, I_mono, I_total, lambda_0, delta_lambda)
+plt.figure(figsize=(10, 2))
+plt.imshow(color_map, extent=[-0.02, 0.02, 0, 1], aspect='auto')
+plt.xlabel(r'$\varphi$, rad')
+plt.yticks([])
+plt.show()
